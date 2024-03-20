3a043cf5e062ff310ed9fe0b385f8f0502714e41
Samm1911 <s_gostanian@hotmail.com>
2024-03-19 23:52:21 -0400

    react

---
+++ src/components/pages/Contact.jsx
@@ -0,0 +1,128 @@
+// import react, use state for the contact form and validate email for email validation
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
+// Contact component
+function Contact() {
+  // declare variables and useState function to create a function to update state
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // get input type and corresponding value for each form input field
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // if condition to check, which type is being input and set the value for corresponding type
+    if (inputType === 'Name') {
+      setName(inputValue);
+    } else if (inputType === 'Email') {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  // function that checks which field is missing and once the cursor moves out it displays, which fields are missing
+  const handleblur = (e) => {
+    if(!name || !email || !message) {
+      // declare array for the missing fields
+      let missingFields = [];
+
+      // push empty values into missingFields array
+      if (!name) {
+        missingFields.push('Name');
+      }
+      if (!email) {
+        missingFields.push('Email');
+      }
+      if (!message) {
+        missingFields.push('Message');
+      }
+
+      // set errormessage which checks which and how many fields are missing, if more than one field is missing the message switches from 'is' to 'are'
+      setErrorMessage(`${missingFields.join(', ')} ${missingFields.length === 1 ? 'is' : 'are'} required`);
+      return;
+    }
+  };
+
+  // once the form is being submitted all the input fields are being cleared
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // if email input doesn't pass validateEmail function display errormessage that email is invalid
+    if(!validateEmail(email)) {
+      setErrorMessage('Email is invalid!');
+      return;
+    }
+    
+    // clear input fields
+    setName('');
+    setEmail('');
+    setMessage('');
+    setErrorMessage('');
+  };
+
+  // display form
+  return (
+    <div className="contact pages">
+      <h2>Contact</h2>
+      <form className='form'>
+        <label htmlFor="name" className="form-label">Name:</label>
+        {/* input name value, with the name "Name", handle inputChange function for single Field input, type text, if cursor moves out handleBlur function, with id and placeholder name */}
+        <input
+          className="form-control form-item"
+          value={name}
+          name="Name"
+          onChange={handleInputChange}
+          onBlur={handleblur}
+          type="text"
+          id="name"
+          placeholder="name"
+        />
+        <label htmlFor="email" className="form-label">Email:</label>
+        {/* input for email */}
+         <input
+          className="form-control form-item"
+          value={email}
+          name="Email"
+          onChange={handleInputChange}
+          onBlur={handleblur}
+          type="email"
+          id="email"
+          placeholder="email"
+        />
+        <label htmlFor="message" className="form-label">Message:</label>
+        {/* textarea field for message */}
+         <textarea
+          className="form-control form-item"
+          rows="6"
+          value={message}
+          name="Message"
+          onBlur={handleblur}
+          id="message"
+          onChange={handleInputChange}
+          placeholder="message"
+        />
+        {/* submit button that handles the handleFormSubmit */}
+        <button type="button" onClick={handleFormSubmit} className='btn btn-primary submit-btn'>Submit</button>
+      </form>
+      <p>Alternatively you can find my email in the footer, once the mail icon is clicked</p>
+      {/* errorMessage in form of a paragraph */}
+      {errorMessage && (
+        <div>
+          <em><p className="error-text">{errorMessage}</p></em>
+        </div>
+      )}
+    </div>
+
+    
+  );
+}
+
+export default Contact;
+
+
---
+++ src/components/pages/Portfolio.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+// import Project components
+import Project from "./Project";
+
+// Portfolio component that uses the Project component with the same structure but different values for each Project
+function Portfolio() {
+  return (
+    <div className="portfolio pages">
+      <h2>Portfolio</h2>
+      <div className="d-flex flex-wrap">
+
+        {/* Film */}
+        <Project
+          className="Film single-project"
+          link="https://samm1911.github.io/Film-Finder/"
+          title="Film Finder"
+          githublink="https://github.com/Samm1911/Film-Finder"
+          description="Film Finder Website"
+        />
+        {/* Workout */}
+        <Project
+          // pass in classes for individual project to handle different backgrounds and single project class
+          className="Workout single-project"
+          // pass link for the deployed app
+          link="https://newworkoutbuddy-app-72d0413499ee.herokuapp.com/"
+          // pass in title of app
+          title="Work Out Buddy App"
+          // pass in corresponding github link
+          githublink="https://github.com/Samm1911/Workoutbuddy-app"
+          // technical description for corresponding app
+          description="Workout Buddy"
+        />
+
+        {/* Weather */}
+        <Project
+          className="Weather single-project"
+          link="https://samm1911.github.io/06-Server-Side-APIs-Weather-Dashboard/"
+          title="Weather Dashboard"
+          githublink="https://github.com/Samm1911/06-Server-Side-APIs-Weather-Dashboard"
+          description="Front-end app with server-side APIs"
+        />
+        {/* Tech Blog */}
+        <Project
+          className="my-tech-blog single-project"
+          link="https://mvc-tech-app-5c80a2d57368.herokuapp.com/"
+          title="Tech Blog"
+          githublink="https://github.com/Samm1911/MVC-Tech-Blog"
+          description="MVC APP"
+        />
+        {/* Social Network API */}
+        <Project
+          className="social-network-api single-project"
+          link="https://drive.google.com/file/d/1cZCW73Xf2bDR7NwB1wG0LGbm1VXSkN0e/view?usp=drive_link"
+          title="Social Network API"
+          githublink="https://github.com/Samm1911/Social-Network-API"
+          description="Back-end Express.js app with MongoDB"
+        />
+        {/* Employee-Tracker
+        <Project
+          className="employee-tracker single-project"
+          link="https://drive.google.com/file/d/1J8Eac3SRjXf4tKRDQbD1gBVwkK4TMqTl/view"
+          title="Employee Tracker"
+          githublink="https://github.com/PythonFabi/my-employee-tracker"
+          description="Node.js App with SQL"
+        /> */}
+        {/* Photography */}
+        <Project
+          className="Photography single-project"
+          link="https://samm1911.github.io/Sam-Gostanian-Photography/"
+          title="Weather dashboard"
+          githublink="https://github.com/Samm1911/Sam-Gostanian-Photography"
+          description="Photography Website"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
