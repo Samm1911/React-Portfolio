769f4af13d51465b7d63b7bd33a7fc41fab85a85
Samm1911 <s_gostanian@hotmail.com>
2024-03-20 18:36:49 -0400

    Updates

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import Portfolio Page component
-import PortfolioPage from "./components/PortfolioPage";
-
-// App renders the PortfolioPage with all the including components
-const App = () => <PortfolioPage />;
-
-export default App;
